88b4b029395d27ffa5b5cf1a62f697336b54127a
cwal1220 <pc31112@gmail.com>
2019-04-25 19:34:57 +0900

    DDL fix

--- Database/Car.sql
+++ Database/Car.sql
@@ -2,54 +2,54 @@ USE doraemon;
 
 
 CREATE TABLE users (
-  usr_id varchar(30) UNIQUE,
-  usr_name varchar(20),
-  token varchar(200)
+  usr_id      VARCHAR (30) UNIQUE,
+  usr_name    VARCHAR(20),
+  token       VARCHAR(200)
 );
 
 
 CREATE TABLE drive (
-  car_id varchar(30),
-  pos_time datetime,
-  fuel_efi double,
-  speed int(11)
+  car_id      VARCHAR(30),
+  pos_time    DATETIME,
+  fuel_efi    DOUBLE,
+  speed       INT(11)
 );
 
 CREATE TABLE position (
-  car_id varchar(30),
-  pos_time datetime,
-  pos_x double,
-  pos_y double
+  car_id      VARCHAR(30),
+  pos_time    DATETIME,
+  pos_x       DOUBLE,
+  pos_y       DOUBLE
 );
 
 CREATE TABLE record (
-  car_id varchar(30),
-  start_time datetime,
-  fuel_efi float,
-  speed int(11),
-  rpm int(11),
-  brk_num int(11),
-  acl_num int(11),
-  score int(11),
-  distance float,
-  end_time datetime
+  car_id      VARCHAR(30),
+  start_time  DATETIME,
+  fuel_efi    FLOAT,
+  speed       INT(11),
+  rpm         INT(11),
+  brk_num     INT(11),
+  acl_num     INT(11),
+  score       INT(11),
+  distance    FLOAT,
+  end_time    DATETIME
 );
 
 CREATE TABLE code (
-  car_id varchar(30) NOT NULL,
-  code_time datetime,
-  code varchar(30) NOT NULL,
-  description varchar(150) NOT NULL
+  car_id      VARCHAR(30) NOT NULL,
+  code_time   DATETIME,
+  code        VARCHAR(30) NOT NULL,
+  description VARCHAR(150) NOT NULL
 );
 
 
 CREATE TABLE car (
-  car_id varchar(30) NOT NULL,
-  usr_id varchar(30) NOT NULL,
-  car_name varchar(30) NOT NULL,
-  volume varchar(30) NOT NULL,
-  fuel varchar(30) NOT NULL,
-  fuel_efi varchar(30) NOT NULL,
+  car_id      VARCHAR(30) NOT NULL,
+  usr_id      VARCHAR(30) NOT NULL,
+  car_name    VARCHAR(30) NOT NULL,
+  volume      VARCHAR(30) NOT NULL,
+  fuel        VARCHAR(30) NOT NULL,
+  fuel_efi    VARCHAR(30) NOT NULL,
   PRIMARY KEY (car_id),
   KEY usr_id (usr_id),
   CONSTRAINT car_ibfk_1 FOREIGN KEY (usr_id) REFERENCES users (usr_id)
