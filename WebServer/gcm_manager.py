cdc9e52d3e405eb35d86fa78abee1041445650b6
Chan Park <cwal1220@naver.com>
2019-08-22 00:33:27 +0900

    replace GCM Librarys

--- WebServer/gcm_manager.py
+++ WebServer/gcm_manager.py
@@ -1,18 +1,9 @@
-from gcm import GCM
+from pyfcm import FCMNotification
 
 def send_gcm_message(token, message):
-	API_KEY = "AAAAB3M5t78:APA91bHX2-CxEDzYUiHRkCyGMXYRHqY3ivZswhl0EosKayO8oFWJYotG3HBIDMG48DwFgZNmV-Vk51MBfhvkpSqtj-9HpawnX98D0ppnLcba4bM7bx3NK9uiT5lsPAOpk1ZADwbjc9AD"
-	gcm = GCM(API_KEY)
-	data = {'message': message}
-
-	# Downstream message using JSON request
-	reg_ids = [token]
-	response = gcm.json_request(registration_ids=reg_ids, data=data)
-
-	# Downstream message using JSON request with extra arguments
-	res = gcm.json_request(registration_ids=reg_ids, data=data, collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600)
-
-
+	API_KEY = "AAAAHkk70d4:APA91bHDIQEnZYM5NPtOM_daip5KX022O-45NKSsHnRT3Om4TaePfaMHltXCe251to1mlnLpDa8SxdARREW5VMTFNYH3TEOJlp9MztyX4l4Pp2L3PRcu7zyaoMVpFki6rukrIsUpkWLu"
+	push_service = FCMNotification(api_key=API_KEY)
+	result = push_service.notify_single_device(	registration_id=token, message_title="주행 종료", message_body=message) # get json or ditc object
 
 if __name__ == "__main__":
-	send_gcm_message("dv2eq2y-QRU:APA91bFdEk02MzZ5atLcNd7PuWy18ztuUAsbX0FfSdqU3Xt784inaM-RvJ9cqhW1g8zvSZhYyOn3CgAb7Tye2yLPnEV-nCOL3wR5gCn9BeSeP0s4TtFTYbXSB_d4Y09UW3XzXZR1x3zJSXJLapl3LBcA-SstXbtBWw", {'fuel_efi':15.3, 'distance':22.2})
+	send_gcm_message("eEjUpeFccz8:APA91bFm_QE2CFtVVmPbbcHQiRR4j7Smt7Nunleq9u9cPChaA4w4bbnbfVw-YEwTRZD9MZHoaGQbmowQQp4egglrhB6Wy1mI32JCBbMhPYTcP75EZKiL57KjCAJrcrjs8y71uQ4QYpFu", {'fuel_efi':15.3, 'distance':22.2})
